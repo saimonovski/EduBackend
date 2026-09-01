573bd67908808077539b2094d0c231cf7b39d3c9
saimonovski <saimonovski069@gmail.com>
2026-09-01 22:44:01 +0200

    updated Tests

--- Test/ClosedQuestionTests/Test1.cs
+++ Test/ClosedQuestionTests/Test1.cs
@@ -52,28 +52,32 @@ public class Test1
     }
    
     [Test]
-    public async  Task TestInCorrectAnswerChecking()
+    public async Task TestInCorrectAnswerChecking()
     {
-            var mainQuestion = TestClosedQuestion.Create();
-        
-            var subQuestion = TestClosedQuestion.Create();
-            subQuestion.Answers = ["incorrect", "correct", "incorrect2"];
-            subQuestion.CorrectAnswers.Add("correct");
-        
-            mainQuestion.Items.Add(subQuestion);
-            mainQuestion.Answers = ["incorrect", "correct", "incorrect2", "incorrect3", "correct2"];
-            mainQuestion.CorrectAnswers.Add("correct");
-            mainQuestion.CorrectAnswers.Add("correct2");
-            
-          await  _questionRepository.Update(mainQuestion);
-          await  _questionRepository.Update(subQuestion);
-            
-            Assert.Multiple(async () =>
-            {
-                Assert.That(await _questionService.CheckAnswers(mainQuestion.Id,"incorrect"), Is.False);
-                Assert.That(await _questionService.CheckAnswers(mainQuestion.Id, "incorrect2"), Is.False);
-                Assert.That(await _questionService.CheckAnswers(subQuestion.Id, "incorrect"), Is.False);
-            });
-        }
+        var mainQuestion = TestClosedQuestion.Create();
+
+        var subQuestion = TestClosedQuestion.Create();
+        subQuestion.Answers = ["incorrect", "correct", "incorrect2"];
+        subQuestion.CorrectAnswers.Add("correct");
+
+        mainQuestion.Items.Add(subQuestion);
+        mainQuestion.Answers = ["incorrect", "correct", "incorrect2", "incorrect3", "correct2"];
+        mainQuestion.CorrectAnswers.Add("correct");
+        mainQuestion.CorrectAnswers.Add("correct2");
+    
+        await _questionRepository.Update(mainQuestion);
+        await _questionRepository.Update(subQuestion);
+    
+        var answer1 = await _questionService.CheckAnswers(mainQuestion.Id, "Incorrect");
+        var answer2 = await _questionService.CheckAnswers(mainQuestion.Id, "incorrect2 ");
+        var answer3 = await _questionService.CheckAnswers(subQuestion.Id, "incorrect");
+    
+        Assert.Multiple(() =>
+        {
+            Assert.That(answer1, Is.False);
+            Assert.That(answer2, Is.False);
+            Assert.That(answer3, Is.False);
+        });
+    }
         
     }
